379a5088f9c53eb696fbf9cb5b14a3b1e1ab3abb
Sasi <sasikiran.tt3011@gmail.com>
2026-03-26 18:45:12 +0530

    feat: establish core application structure, routing, and main content pages.

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Solutions from './pages/Solutions';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Consultation from './pages/Consultation';
+import Blog from './pages/Blog';
+
+
+// Scroll to top helper
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/solutions" element={<Solutions />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/consultation" element={<Consultation />} />
+
+          {/* Default Route */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
---
+++ tmp_check_icons.js
@@ -0,0 +1,3 @@
+import * as Icons from 'lucide-react';
+console.log(Object.keys(Icons).filter(icon => icon.toLowerCase().includes('insta')));
+console.log(Object.keys(Icons).filter(icon => icon.toLowerCase().includes('linke')));
